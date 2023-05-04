b48031e3ad905f66e43ccb6735fc72b45c62eae0
Mitsuhiro Kawashima <mshimashima@gmail.com>
2023-05-04 21:19:43 +0900

    トップページ実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
-import { Inter } from 'next/font/google';
+import Card from '@/components/Card';
+import Layout from '@/components/Layout';
+import { siteConfig } from '@/site.config';
+import { sampleCards } from '@/utils/sample';
+import { NextPage } from 'next';
 
-const inter = Inter({ subsets: ['latin'] });
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <div className="pt-12">
+        <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
+        <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
+          {sampleCards.map((page, index) => (
+            <Card key={index} page={page} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default function Home() {
-  return <></>;
-}
+export default Home;
---
+++ site.config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: 'Notion Next Blog',
+  twitterURL: 'http://twitter.com',
+  instagramUrl: 'http://instagram.com',
+  githubUrl: 'http://github.com',
+};
